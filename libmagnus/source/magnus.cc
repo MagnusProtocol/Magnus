e2b88bc71d749d41ee1d71c8a4d509456018dcba
SteveGremory <stevegremory2006@gmail.com>
2022-06-16 00:58:49 +0530

    Added a MMAP Wrapper alongwith an example.

--- libmagnus/source/magnus.cc
+++ libmagnus/source/magnus.cc
@@ -1,14 +1,8 @@
 #include "magnus.hh"
 #include <filesystem>
 #include <string.h>
-
-// For mmap
-#include <cstring>
-#include <exception>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include "utils.hh"
 
 namespace Magnus::LibMagnus {
+
 }
